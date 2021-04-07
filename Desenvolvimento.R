f7f13d5e87ba8fc5253ce39fe2158d6b464f991d
Luiz Rothier <luizrothier@gmail.com>
2021-04-07 19:26:26 -0300

    Atualização de 07abr21

--- Desenvolvimento.R
+++ Desenvolvimento.R
@@ -3,18 +3,29 @@ dirDeTrabalho
 arquivosDoExercio <- list.files("./exercicios/exercicios/dados_ex1")
 arquivosDoExercio
 
-resultado <- arquivosDoExercio %>% enframe(name = arquivosDoExercio) %>% mutate(
-    map(
-        str_glue(
-            unnest(
-                str_remove(
-                    mutate(
-                        as.integer()
-                    )
-                )
-            )
-        )
-    )
-)
+teste <- enframe(arquivosDoExercio, name = "name", value = "value")
+teste
 
-resultado
+resultado <- tibble(arquivo = list.files("./exercicios/exercicios/dados_ex1")) %>%
+    #view()
+    mutate(arquivo = str_glue("./exercicios/exercicios/dados_ex1/{arquivo}")) %>%
+    #view()
+    mutate(conteudo = map(arquivo,read_csv)) %>% 
+    unnest(conteudo)
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# plan(multiprocess)
+# 
+# todos_os_fundos <- tibble(arquivo = list.files("dados/fundos")) %>%
+#     mutate(arquivo = str_glue("dados/fundos/{arquivo}")) %>%
+#     mutate(conteudo = future_map(arquivo, read_rds , .progress = TRUE)) %>% 
+#     unnest(conteudo)
+# 
+# head(todos_os_fundos)
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+#caminho <- "./exercicios/exercicios/dados_ex1"
+#arquivo <- "2015.csv"
+caminhoArquivo <- str_glue("./exercicios/exercicios/dados_ex1/2015.csv")
+conteudo <- read.csv(caminhoArquivo)
+data.frame(conteudo)
